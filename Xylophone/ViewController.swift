8510082354b9625d80d2d82138351902e1b50ae8
Shrey's Macbook Pro <shhr3y@gmail.com>
2020-06-22 05:52:19 +0530

    Final App Completed

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let url = Bundle.main.url(forResource: sender.currentTitle!, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            UIView.animate(withDuration: 0.3) {
+                sender.alpha = 1.0
+            }
+        }
+    }
+}
+ 
